e9cfa9e707e5471d56cc77f4f85a1ef74e9b4779
catgrev <catgrev@5c2a22ae-e9a5-f6c8-f00f-893a186c648d>
2010-11-28 22:19:56 +0000

    Matched the mouse interaction with the eye interaction

--- EyeTracker.java
+++ EyeTracker.java
@@ -5,17 +5,20 @@ import java.awt.BorderLayout;
 import java.awt.event.*;
 import java.io.*;
 import java.awt.Rectangle;
+import java.awt.*;
 
 public class EyeTracker
 {
-        // If true, use mouse input instead of opengazer.
-        boolean debug = false;
-        private EyeTrackerComponent comp = null;
+    // If true, use mouse input instead of opengazer.
+   	boolean debug = true;
+    private EyeTrackerComponent comp = null;
+    private JFrame frame;
+    private JScrollPane scrollArea;
 
 	public EyeTracker(){
 	
 		// Initialize the overall JFrame window
-		JFrame frame = new JFrame("Courier Demo");
+		frame = new JFrame("Courier Demo");
 	 	frame.setSize(1400,800);
 	 	frame.setLayout(new BorderLayout());
 	 	frame.addWindowFocusListener( new WindowFocusListener(){
@@ -32,40 +35,30 @@ public class EyeTracker
     	});
     	
     	JPanel navigation = new JPanel();
-    	JButton button = new JButton("help");
-    	
+    	//JButton button = new JButton("ALERT!");
+    	//button.setVisible(false);
 	 	String[] files = {"BenjaminButton.txt", "Emma.txt"};
 	 	JComboBox selectText = new JComboBox(files);
 	 	navigation.add(selectText, BorderLayout.NORTH);
-	 	navigation.add(button, BorderLayout.NORTH);
+	 	//navigation.add(button, BorderLayout.NORTH);
 	 	frame.add(navigation, BorderLayout.NORTH);
 	 	
 	 	selectText.addActionListener(new ActionListener(){
 	 		public void actionPerformed(ActionEvent e) {
         		JComboBox cb = (JComboBox)e.getSource();
-        		String petName = (String)cb.getSelectedItem();
+        		String textFile = (String)cb.getSelectedItem();
+        		if(comp != null){
+	 				comp.updateText(getTextFromFile(textFile));
+	 			}
         	}
 	 	});
 	 	
-	 	String testText = "";
-	 	try{
-	 		FileReader fro = new FileReader( "ExampleTexts/BenjaminButton.txt" );
-	 		BufferedReader in = new BufferedReader(fro);
-			String stringRead = in.readLine();
-	 		while(stringRead != null){
-	 			testText += stringRead;
-	 			stringRead = in.readLine();
-	 		}
-	 	}catch(Exception e){
-	 		System.out.println(e);
-	 	}
-	 	
 	 	// Create the new EyeTrackerComponent
-	 	comp = new EyeTrackerComponent(testText);
+	 	comp = new EyeTrackerComponent(getTextFromFile("BenjaminButton.txt"));
 	 	comp.setPreferredSize(new Dimension(frame.getWidth()-20,1400));
 	 	
 	 	// Add the eyeTrackerComponent to a scrollable area
-	 	JScrollPane scrollArea = new JScrollPane(comp, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED );	 	
+	 	scrollArea = new JScrollPane(comp, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED );	 	
 	 	scrollArea.setPreferredSize(new Dimension(frame.getWidth(),frame.getHeight()));
 
 		// Add the scroll area to the main frame
@@ -81,7 +74,14 @@ public class EyeTracker
                         {
                             try
                             {
-                                comp.setPosition(e.getX(), e.getY());
+                            int x = e.getX();
+		   					int y = e.getY();
+		   					comp.setPosition(x, y);
+		   		
+		   					// Scroll the scroll pane if the eye position is at the very bottom of the screen.
+		   					if(y > (frame.getHeight()/7)*6){
+		   						scrollArea.getVerticalScrollBar().setValue(scrollArea.getVerticalScrollBar().getValue()+20);
+		   					}
                             }catch (NumberFormatException nfe){
                                 System.out.println("NumberFormatException: " + nfe.getMessage());
                             }
@@ -106,14 +106,34 @@ public class EyeTracker
 		   		if(y > (frame.getHeight()/7)*6){
 		   			scrollArea.getVerticalScrollBar().setValue(scrollArea.getVerticalScrollBar().getValue()+20);
 		   		}
+		   		/*if(y < 100){
+		   			button.setVisible(true);
+		   		}else{
+		   			navigation.setBackground(Color.gray);
+		   		}*/
 		   		
 		   }catch (NumberFormatException nfe){
       			System.out.println("NumberFormatException: " + nfe.getMessage());
-                   }
-                 }
-                }
-    	
+           }
+         }
+       }
 	}
+    
+    public String getTextFromFile(String file){
+    	String returnString = "";
+    	try{
+	 		FileReader fro = new FileReader( "ExampleTexts/"+file );
+	 		BufferedReader in = new BufferedReader(fro);
+			String stringRead = in.readLine();
+	 		while(stringRead != null){
+	 			returnString += stringRead;
+	 			stringRead = in.readLine();
+	 		}
+	 	}catch(Exception e){
+	 		System.out.println(e);
+	 	}
+    	return returnString;
+    }
     
     public static void main(String[] args )
     {
