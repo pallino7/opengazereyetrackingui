e57ad9b23acbf42a2cec00013141c2ef0b10c073
catgrev <catgrev@5c2a22ae-e9a5-f6c8-f00f-893a186c648d>
2010-11-29 18:11:22 +0000

    added navigation bar alert feature and reset button

---
+++ trunk/GazePoint.java
@@ -0,0 +1,12 @@
+public class GazePoint{
+     public int x;
+     public int y;
+     public long time;
+
+     public GazePoint(int a, int b, long t)
+     {
+          x = a;
+          y = b;
+          time = t;
+     }
+}
